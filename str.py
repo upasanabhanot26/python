1c1a3946da76d09da796f87228861e56734dc7c9
upasanabhanot26 <35559278+upasanabhanot26@users.noreply.github.com>
2018-01-24 15:10:12 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,14 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x='hello world'
+>>> x
+'hello world'
+>>> x+"hello people"
+'hello worldhello people'
+>>> x[0]
+'h'
+>>> x[1]
+'e'
+>>> x[0:2]
+'he'
+>>> 
